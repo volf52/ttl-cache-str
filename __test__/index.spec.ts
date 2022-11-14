141bd9365bc37950522ffe67c05cb9eae1e017e6
Arslan <rslnkrmt2552@gmail.com>
2022-11-14 23:09:05 +0500

    0.0.1: fix tests

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,7 +1,9 @@
-import { it, expect } from "@jest/globals";
+import { it, expect, describe } from "@jest/globals";
 
 import { sum } from "../index.js";
 
-it("sum from native", (t) => {
-	expect(sum(1, 2)).toBe(3);
+describe("sum", () => {
+	it("sum from native", () => {
+		expect(sum(1, 2)).toBe(3);
+	});
 });
